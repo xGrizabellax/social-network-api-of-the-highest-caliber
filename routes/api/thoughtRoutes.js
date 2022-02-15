56de9a682a8fcc7a460162fdfff5c0b581b1d706
Sam Page <sampage6@gmail.com>
2022-02-14 23:39:12 -0600

    updating more routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,14 +1,19 @@
 const router = require('express').Router();
 const {
+  // Thought controllers
   getThoughts,
   getSingleThought,
-  createThought,
+  makeThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction
 } = require('../../controllers/thoughtController');
 
-// /api/users
-router.route('/').get(getThoughts).post(createThought);
-
-// /api/users/:userId
-router.route('/:thoughtId').get(getSingleThought);
+// Routes for thoughts
+router.route('/').get(getThoughts).post(makeThought);
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId/reactions').post(addReaction);
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,14 +1,13 @@
-
-   
 const router = require('express').Router();
 const {
   getUsers,
   getSingleUser,
   createUser,
+  addFriend,
+  removeFriend,
   updateUser,
   deleteUser,
-  addFriend,
-  removeFriend
+
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
