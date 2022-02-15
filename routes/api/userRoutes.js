1142c34f1cba42d8cd0b0a5a5fef828d993f3acd
Sam Page <sampage6@gmail.com>
2022-02-14 23:38:45 -0600

    updated userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,12 +1,20 @@
+
+   
 const router = require('express').Router();
 const {
-  getSingleUser,
   getUsers,
+  getSingleUser,
   createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
 } = require('../../controllers/userController');
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getSingleUser);
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend)
 
 module.exports = router;
