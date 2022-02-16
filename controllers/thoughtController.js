aa61a65f6c29d50a32690db8e2a394e446ab731a
Sam Page <sampage6@gmail.com>
2022-02-15 18:17:09 -0600

    updated thought controllers with add reaction

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,44 +1,99 @@
 const { Thought, User } = require('../models');
 
+
+
+const thoughtCount = async () =>
+  Thought.aggregate()
+    .count('thoughtCount')
+    .then((numberOfThoughts) => numberOfThoughts);
+
+
 module.exports = {
+
+
   getThoughts(req, res) {
-    Thought.find({})
-      .select('-__v')
-      .populate('users')
-      .then((thoughts) => res.json(thoughts))
+    Thought.find()
+      .then(async (thoughts) => {
+        const thoughtObj = {
+          thoughtCount: await thoughtCount(),
+          thoughts
+        };
+        return res.json(thoughtObj);
+      })
       .catch((err) => res.status(500).json(err));
   },
+
+
   getSingleThought(req, res) {
     Thought.findOne({ _id: req.params.thoughtId })
       .select('-__v')
-      .populate('users')
       .then((thought) =>
         !thought
-          ? res.status(404).json({ message: 'No thought with that ID' })
-          : res.json(thought)
-      )
-      .catch((err) => res.status(500).json(err));
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(thought))
+      .catch((err) => res.status.json(err))
   },
-  // create a new thought
-  createThought(req, res) {
+
+
+  makeThought(req, res) {
     Thought.create(req.body)
       .then((thought) => {
+        // First find a user and then update it to add a thought
         return User.findOneAndUpdate(
-          { _id: req.body.userId },
+          { username: req.body.username },
           { $addToSet: { thoughts: thought._id } },
           { new: true }
         );
       })
       .then((user) =>
         !user
-          ? res
-              .status(404)
-              .json({ message: 'Thought created, but found no user with that ID' })
-          : res.json('Created the thought 🎉')
-      )
+          ? res.status(404).json({ message: 'Thought thunk, but found no user with that username' })
+          : res.json('Created the thought 🧠'))
       .catch((err) => {
         console.log(err);
         res.status(500).json(err);
-      });
+      })
   },
-};
+
+
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { new: true })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with this ID' })
+          : res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+
+
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought found with that ID' })
+          : res.json(thought))
+      .catch((err) => res.status(500).json(err))
+  },
+
+
+  addReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { new: true })
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err))
+  },
+
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { new: true })
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err))
+  }
+}
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const {
-  // Thought controllers
   getThoughts,
   getSingleThought,
   makeThought,
@@ -10,7 +9,7 @@ const {
   deleteReaction
 } = require('../../controllers/thoughtController');
 
-// Routes for thoughts
+// The many thought routes. Each function is an option of what can do depending on the request type
 router.route('/').get(getThoughts).post(makeThought);
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 router.route('/:thoughtId/reactions').post(addReaction);
